19fdd862d0e228f87e60aafa441c89fa36e10670
Nouran <nearat.nouran@gmail.com>
2018-08-17 11:55:15 +0000

    adding deskew function

--- sample/build/exec.sh
+++ sample/build/exec.sh
@@ -4,6 +4,7 @@ g++ -c ../src/program.cpp
 
 g++ program.o /usr/local/lib/libespeak-ng.so \
 -lpulse-simple -lpulse -llept -ltesseract -lraspicam \
--lopencv_core -lopencv_imgproc -lopencv_objdetect -o main
+-lopencv_core -lopencv_imgproc -lopencv_objdetect -lopencv_highgui -o main
 
+# find / | grep search
 #./main ../img/arabic2.png
--- sample/src/program.cpp
+++ sample/src/program.cpp
@@ -12,7 +12,7 @@
 #include <raspicam/raspicam.h>
 #include <ctime>
 #include <unistd.h>
-#include <opencv/cv.h>
+#include <opencv2/opencv.hpp>
 
 
 tesseract::TessBaseAPI *api;
@@ -124,6 +124,63 @@ void captureImage(){
 	
 	}
 
+Mat deskew(double angle, Mat img){
+  
+  std::vector<cv::Point> points;
+  cv::Mat_<uchar>::iterator it = img.begin<uchar>();
+  cv::Mat_<uchar>::iterator end = img.end<uchar>();
+  for (; it != end; ++it)
+    if (*it)
+      points.push_back(it.pos());
+ 
+  cv::RotatedRect box = cv::minAreaRect(cv::Mat(points));
+  cv::Mat rot_mat = cv::getRotationMatrix2D(box.center, angle, 1);
+  cv::Mat rotated;
+  cv::warpAffine(img, rotated, rot_mat, img.size(), cv::INTER_CUBIC);
+  
+  cv::Size box_size = box.size;
+  if (box.angle < -45.)
+    std::swap(box_size.width, box_size.height);
+  cv::Mat cropped;
+  cv::getRectSubPix(rotated, box_size, box.center, cropped);
+   
+  bitwise_not(cropped, cropped);
+ 
+  return cropped;
+  }
+
+double compute_skew(Mat src) {
+   
+   cv::threshold(src, src, 127, 255, cv::THRESH_BINARY);
+   AdaptiveThreshold(src,src,255,ADAPTIVE_THRESH_GAUSSIAN_C,CV_THRESH_BINARY,13,0);
+
+
+
+   cv::Size size = src.size();
+   cv::bitwise_not(src, src);
+   std::vector<cv::Vec4i> lines;
+   cv::HoughLinesP(src, lines, 1, CV_PI/180, 100, size.width / 2.f, 20);
+   cv::Mat disp_lines(size, CV_8UC1, cv::Scalar(0, 0, 0));
+   double angle = 0.;
+   unsigned nb_lines = lines.size();
+   for (unsigned i = 0; i < nb_lines; ++i) {
+        cv::line(disp_lines, cv::Point(lines[i][0], lines[i][1]),
+                 cv::Point(lines[i][2], lines[i][3]), cv::Scalar(255, 0 ,0));
+        angle += atan2((double)lines[i][3] - lines[i][1],
+                       (double)lines[i][2] - lines[i][0]);
+    }
+  angle /= nb_lines; // mean angle, in radians.
+  printf("skew angle in degrees: %f \n", angle * 180 / CV_PI );
+  return angle * 180 / CV_PI;
+}
+
+void imageProcessing(char* argv[]){
+	cv::Mat src = cv::imread(argv[1], 0);
+	double degrees = compute_skew(src);
+	Mat result= deskew(degrees, src);
+    imwrite( argv[2], result );	
+	}
+
 int main(int argc, char* argv[]) {
 
 	setupPulseAudio();
@@ -132,7 +189,8 @@ int main(int argc, char* argv[]) {
 	setupEspeak();
 	
 	//captureImage();
-	
+	//imageProcessing();
+  
 	Pix *image = pixRead(argv[1]);
 	api->SetImage(image);
 	char *text;
@@ -140,7 +198,6 @@ int main(int argc, char* argv[]) {
 	printf("OCR output:\n%s", text);
 
 	unsigned int size = strlen(text) + 1;
-
 	espeak_POSITION_TYPE positionType = POS_WORD;
 		
 	espeak_Synth(text, size, position, positionType, endPosition, flags,	uniqueIdentifier, userData);
